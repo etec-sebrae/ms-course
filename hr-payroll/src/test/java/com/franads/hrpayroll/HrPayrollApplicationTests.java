b4da432234aa77debf91376597322ba86485c51b
Francisco Aurizelio <francisco.souza63@etec.sp.gov.br>
2021-03-03 02:17:18 -0300

    hr-payroll create

---
+++ hr-payroll/src/test/java/com/franads/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franads.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
